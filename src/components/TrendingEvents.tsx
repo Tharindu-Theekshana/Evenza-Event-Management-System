5b37c535634f83544be40a6495559c78e0d2f3f6
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-06-29 22:22:02 +0530

    Update TrendingEvents.tsx

--- src/components/TrendingEvents.tsx
+++ src/components/TrendingEvents.tsx
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {getAllApprovedEvents} from '../services/EventService';
 
 const TrendingEvents = () => {
 
     const [events, setEvents] = useState([]);
     const [showAll, setShowAll] = useState(false);
+
+
+    useEffect(()=>{
     
+        const fetchEvents = async () => {
+         try{
+
+            const data = await getAllApprovedEvents();
+            setEvents(data);
+
+            }catch(e){
+            console.error("failed to load events : ", e);
+            }
+        };
+        fetchEvents();
+    },[]);
 
 
     return(

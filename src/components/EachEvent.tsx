5fcd68f4963cd85b8e1a2cbc5ecb836ef89999cf
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-07-11 13:00:19 +0530

    Update EachEvent.tsx

--- src/components/EachEvent.tsx
+++ src/components/EachEvent.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { getEventById } from "../services/EventService";
-import { number } from "yup";
+
 
  const EachEvent = () => {
 
